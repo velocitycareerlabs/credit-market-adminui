c91508b47967c7347eea4a961ba0b6e9e9bfee36
Deepika <dcsodani@gmail.com>
2022-04-21 15:23:30 +0530

    [Mifos] Add UI to edit the savings accounts dates https://velocitycareerlabs.atlassian.net/browse/VL-2113

--- app/scripts/modules/configurations.js
+++ app/scripts/modules/configurations.js
@@ -1,9 +1,9 @@
 angular.module('configurations', ['auth0.auth0'])
     .config(function(angularAuth0Provider){
-        var domain = '$AUTH0_SERVER_URL';
-        var clientID = '$AUTH0_CLIENT_ID';
-        var connection = '$AUTH0_CONNECTION_ID';
-        var audience = '$AUTH0_AUDIENCE';
+        var domain = 'vnf-dev.us.auth0.com';
+        var clientID = 'RtFUVDUH9oWRVnCS0S5XkDFhZThX141w';
+        var connection = 'vnf-dev-users-connection';
+        var audience = 'https://fineract.velocitycareerlabs.io';
         var redirecturi = window.location.origin;
 
         angularAuth0Provider.init({
@@ -18,8 +18,8 @@ angular.module('configurations', ['auth0.auth0'])
     })
     .constant('API_VERSION', '/fineract-provider/api/v1')
     .constant('SCOPES', 'openid profile fineract:super_user fineract:operations')
-    .constant('FINERACT_BASE_URL', '$FINERACT_BASE_URL')
-    .constant('AUDIENCE', '$AUTH0_AUDIENCE')
+    .constant('FINERACT_BASE_URL', 'https://localhost:8443')
+    .constant('AUDIENCE', 'https://fineract.velocitycareerlabs.io')
     .constant('IDLE_DURATION', 30 * 60)
     .constant('WARN_DURATION', 10)
     .constant('KEEPALIVE_INTERVAL', 15 * 60)
